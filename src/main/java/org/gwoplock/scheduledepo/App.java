5ca0f1973e2da959adc6b9f5839c8b631ea72b72
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-07-19 08:28:54 -0500

    maven build added

---
+++ src/main/java/org/gwoplock/scheduledepo/App.java
@@ -0,0 +1,13 @@
+package org.gwoplock.scheduledepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
